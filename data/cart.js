cca4ba015950da4283f73faccfbb63b122e3b688
v392 <81261845+v392@users.noreply.github.com>
2025-12-14 13:51:14 +0200

    Refactor cart initialization and functions
    Removed initial cart items and retained cart functions.

--- data/cart.js
+++ data/cart.js
@@ -2,24 +2,7 @@ import {timeString} from "../scripts/checkout_sub/orderSummary.js";
 import {orderTimeFormat} from "./orders_data.js";
 import {trackingTimeFormat} from "../scripts/tracking.js";
 
-export let cart = loadCart() || [
-  {
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    productQuantity: 1,
-    deliveryOptionId: 1,
-    deliveryDate: timeString(dayjs().add(7, 'day')),
-    orderDeliveryDate: orderTimeFormat(dayjs().add(7, 'day')) ,
-    trackingDeliveryDate: trackingTimeFormat(dayjs().add(7, 'day'))
-  },
-  {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    productQuantity: 2,
-    deliveryOptionId: 2,
-    deliveryDate: timeString(dayjs().add(3, 'day')),
-    orderDeliveryDate: orderTimeFormat(dayjs().add(3, 'day')),
-    trackingDeliveryDate: trackingTimeFormat(dayjs().add(3, 'day'))
-  }
-];
+export let cart = loadCart() || [];
 
 export function saveCart () {
   localStorage.setItem('cart', JSON.stringify(cart));
@@ -28,4 +11,5 @@ export function loadCart () {
   if (JSON.parse(localStorage.getItem('cart'))){
     return JSON.parse(localStorage.getItem('cart'));
   };
+
 };
